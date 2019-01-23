4aaecf3dfd95a8bba31002aec946174004c1ba4f
Oliver <oliver.grant@gmail.com>
2019-01-23 19:17:10 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,23 +11,23 @@ def third_challenge
         },
       :hero => {
         name: "Romeo",
-        age: "15"
+        age: "15",
         statue: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet"
+        name: "Lord Capulet",
         age: "50"
         },
       :matriarch => {
-        name: "Lady Capulet"
+        name: "Lady Capulet",
         age: "51"
       },
       :heroine => {
-        name: "Juliet"
-        age: "15"
+        name: "Juliet",
+        age: "15",
         statue: "alive"
         },
       :heroine_friends => []
