4072f4771c18c78de519eaab1891482269a0d29c
Oliver <oliver.grant@gmail.com>
2019-01-23 19:11:23 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,7 +3,7 @@ def second_challenge
     :montague => {
       :patriarch => {},
       :matriarch => {},
-      :helper_method => {},
+      :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
